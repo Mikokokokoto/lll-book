9be30dbb7fed187bc997a521cd895b3c9b683974
Mikokokokoto <58349362+Mikokokokoto@users.noreply.github.com>
2021-09-29 17:16:46 -0400

    test update

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
+import pandas as pd
+import utility as util
+from datetime import datetime
+
 if __name__ == "__main__":
-	print("test")
+	df = pd.read_csv("book_with_grids.csv")
+	for i in range(30):
+		all_fields = df[df['grid']==i]['fields']
+		if len(all_fields) != 0:
+			print(all_fields.values[0].split(','))
+	
+		# for idx, field in enumerate(all_fields):
+		# 	print(idx)
+		# 	print(field)
+	
