932ce141225dc47e33158733d4b10aaacd929ebf
Mikokokokoto <58349362+Mikokokokoto@users.noreply.github.com>
2021-09-29 18:23:14 -0400

    update

--- heatmap.py
+++ heatmap.py
@@ -3,23 +3,42 @@ import utility as util
 from datetime import datetime
 
 GRID = [[0]*20]*10
-s= datetime(2021,1,1) # start datetime
-e= datetime(2021,9,20) # end datetime
+
+
+def check_fields(fields, all_fields):
+	for idx, field in enumerate(all_fields):
+		if field in given_fields:
+			return True, idx
+	return False, idx
 
 def generate_linklab_heatmap(start_datetime, end_datetime
 , fields, export_filepath):
 	df = pd.read_csv("book_with_grids.csv")
-	for idx in range(200):
-		fields = df[df['grid']==idx]['fields']
-		if len(fields) != 0:
-			for field in fields:
-				lst = list(df[df['grid']==2]['device_id'])
-				print(len(util.get_lfdf(field, s, e, lst)))
+
+	for n in range(200):
+		all_fields = df[df['grid']==n]['fields']
+		field_lst = all_fields.values[0].split(',')
+		condition, param_index = check_fields(fields, field_lst)
+		if len(fields) != 0 and condition:
+			parameter = field_lst[param_index]
+			entry = util.get_lfdf(parameter, start_datetime, end_datetime, list(df[df['grid']==n]['device_id']))
+			entry_num = len(entry)
+			GRID[n//20][n%20] += entry_num
 
 
 
+
+
+
+			
+
+
+fields --> devices 
 
 
 if __name__ == "__main__":
+	s= datetime(2021,8,20) # start datetime
+	e= datetime(2021,9,20) # end datetime
+
 	generate_linklab_heatmap(s, e, 0, 0)
 	
--- test.py
+++ test.py
@@ -3,13 +3,19 @@ import utility as util
 from datetime import datetime
 
 if __name__ == "__main__":
-	df = pd.read_csv("book_with_grids.csv")
-	for i in range(30):
-		all_fields = df[df['grid']==i]['fields']
-		if len(all_fields) != 0:
-			print(all_fields.values[0].split(','))
-	
+	# df = pd.read_csv("book_with_grids.csv")
+	# fields = []
+	# for element in df.itertuples():
+	# 	lst = (element[6].split(','))
+	# 	for element in lst:
+	# 		if element not in fields:
+	# 			fields.append(element)
+	# print(fields)
 		# for idx, field in enumerate(all_fields):
 		# 	print(idx)
 		# 	print(field)
-	
+		
+	# lst = ['Illumination_lx', 'Range select', 'Supply voltage_V', 
+	# 'rssi', 'Humidity_%', 'Temperature_°C', 'awair_score', 'co2_ppm', 
+	# 'pm2.5_μg/m3', 'voc_ppb', 'T-Sensor', 'PIR Status', 'Supply voltage (OPTIONAL)_V', 
+	# 'Supply voltage availability', 'Contact', 'Concentration_ppm', 'H-Sensor']
