3a0dd7591f8ab6ae77a3ead49ae86123b31696e3
Mikokokokoto <58349362+Mikokokokoto@users.noreply.github.com>
2021-09-29 19:26:29 -0400

    successful small trial

--- heatmap.py
+++ heatmap.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import utility as util
 from datetime import datetime
+import numpy as np
 
-GRID = [[0]*20]*10
+GRID = np.zeros((10,20)).astype(int)
+
 
 
 def check_fields(fields, all_fields):
 	for idx, field in enumerate(all_fields):
-		if field in given_fields:
+		if field in fields:
 			return True, idx
 	return False, idx
 
@@ -17,28 +19,30 @@ def generate_linklab_heatmap(start_datetime, end_datetime
 
 	for n in range(200):
 		all_fields = df[df['grid']==n]['fields']
-		field_lst = all_fields.values[0].split(',')
-		condition, param_index = check_fields(fields, field_lst)
-		if len(fields) != 0 and condition:
-			parameter = field_lst[param_index]
-			entry = util.get_lfdf(parameter, start_datetime, end_datetime, list(df[df['grid']==n]['device_id']))
-			entry_num = len(entry)
-			GRID[n//20][n%20] += entry_num
+		
+		if len(all_fields) != 0:
+			field_lst = all_fields.values[0].split(',')
+			condition, param_index = check_fields(fields, field_lst)
+			if condition:
+				parameter = field_lst[param_index]
+				entry = util.get_lfdf(parameter, start_datetime, end_datetime, list(df[df['grid']==n]['device_id']))
+				if isinstance(entry, pd.DataFrame):
+					entry_num = len(entry)
+					GRID[n//20][n%20] += entry_num
 
 
 
-
-
-
-			
-
-
-fields --> devices 
 
 
 if __name__ == "__main__":
-	s= datetime(2021,8,20) # start datetime
-	e= datetime(2021,9,20) # end datetime
+	s= datetime(2021,9,1) # start datetime
+	e= datetime(2021,9,23) # end datetime
 
-	generate_linklab_heatmap(s, e, 0, 0)
+	fields = ['Illumination_lx', 'Range select', 'Supply voltage_V', 
+	'rssi', 'Humidity_%', 'Temperature_°C', 'awair_score', 'co2_ppm', 
+	'pm2.5_μg/m3', 'voc_ppb', 'T-Sensor', 'PIR Status', 'Supply voltage (OPTIONAL)_V', 
+	'Supply voltage availability', 'Contact', 'Concentration_ppm', 'H-Sensor']
+
+	generate_linklab_heatmap(s, e, fields, 0)
+	print(GRID)
 	
--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import utility as util
 from datetime import datetime
+import numpy as np
+
+GRID = np.zeros((3, 10))
 
 if __name__ == "__main__":
 	# df = pd.read_csv("book_with_grids.csv")
+	for i in range(20):
+		GRID[i//10][i%10] += 1
+		print(GRID)
+
 	# fields = []
 	# for element in df.itertuples():
 	# 	lst = (element[6].split(','))
@@ -14,7 +21,7 @@ if __name__ == "__main__":
 		# for idx, field in enumerate(all_fields):
 		# 	print(idx)
 		# 	print(field)
-		
+
 	# lst = ['Illumination_lx', 'Range select', 'Supply voltage_V', 
 	# 'rssi', 'Humidity_%', 'Temperature_°C', 'awair_score', 'co2_ppm', 
 	# 'pm2.5_μg/m3', 'voc_ppb', 'T-Sensor', 'PIR Status', 'Supply voltage (OPTIONAL)_V', 
